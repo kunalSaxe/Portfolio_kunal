f2740e082b91e5a08cb2a052ab4e5fd2cc19e172
Kunal Saxena <kunalsaxena@Mac.lan>
2025-02-07 00:51:03 +0530

    new portfolio

---
+++ app/components/About.tsx
@@ -0,0 +1,68 @@
+"use client";
+
+import { SectionWrapper } from "./HigherOrderComponents";
+import { motion } from "framer-motion";
+import Image from "next/image";
+import { Tilt } from "react-tilt";
+import { services } from "../constants";
+import { fadeIn, textVariant } from "@/app/utils/motion";
+
+type ServiceCardProps = {
+	index: number;
+	title: string;
+	icon: string;
+};
+
+const ServiceCard = ({ index, title, icon }: ServiceCardProps) => {
+	return (
+		<>
+			<Tilt
+				options={{ max: 45, scale: 1, speed: 450 }}
+				className="xs:w-[250px] w-full"
+			>
+				<motion.div
+					variants={fadeIn("right", "spring", 0.5 * index, 0.75)}
+					className="w-full green-pink-gradient p-px rounded-[20px] shadow-card"
+				>
+					<div className="bg-tertiary rounded-[20px] py-5 px-12 min-h-[280px] flex justify-evenly items-center flex-col">
+						<Image
+							src={icon}
+							width={64}
+							height={64}
+							alt={title}
+							className="w-16 h-16 object-contain"
+						/>
+						<h3 className="text-white text-[20px] font-bold text-center">
+							{title}
+						</h3>
+					</div>
+				</motion.div>
+			</Tilt>
+		</>
+	);
+};
+
+const About = () => {
+	return (
+		<>
+			<motion.div variants={textVariant()}>
+				<p className="sectionSubText">Introduction</p>
+				<h2 className="styles.sectionHeadText">Overview.</h2>
+			</motion.div>
+
+			<motion.p
+				variants={fadeIn("", "", 0.1, 1)}
+				className="mt-4 text-secondary text-[17px] max-w-[3xl] leading-[30px]"
+			>
+				Motivated individual with a strong foundation in development tools and data engineering, seeking a position in a growth-oriented company. Proficient in MySQL and experienced in building websites, e-commerce platforms, and stock management systems. Completed a Full Stack Developer internship with Numeric Infosystem Pvt Ltd, working on live projects. Successfully completed a data engineering project as a mutual fund broker, showcasing expertise in data processing and management. Pursuing BTech in Computer Science. Certified in MERN Stack Development. Fluent in English, Hindi, and French, with excellent interpersonal and organizational skills. Highly motivated, self-driven, and dedicated to continuous learning and growth in the field of data engineering.
+			</motion.p>
+			<div className="mt-20 flex flex-wrap gap-10">
+				{services.map((service, index) => (
+					<ServiceCard key={service.title} index={index} {...service} />
+				))}
+			</div>
+		</>
+	);
+};
+
+export default SectionWrapper(About, "about");
